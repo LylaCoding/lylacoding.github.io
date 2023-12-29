45ff3cd73de8c1443193261a4502b6d71e577bfa
Lou <90841974+LouCodingStuff@users.noreply.github.com>
2023-12-29 01:23:55 +0000

    MCU Timeline Project

---
+++ MCU/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+    background(0);
+    stroke(255, 151, 33)
+    strokeWeight(10)
+    line(0, (windowHeight / 2), windowWidth, windowHeight / 2)
+}
