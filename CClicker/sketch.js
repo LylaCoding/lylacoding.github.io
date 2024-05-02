e50ca0dd9881502709f1b2c6e4ec085fd373ecd8
NotLouis05 <90841974+NotLouis05@users.noreply.github.com>
2024-05-02 04:07:45 +0100

    game kinda complete yippee

--- CClicker/sketch.js
+++ CClicker/sketch.js
@@ -6,7 +6,7 @@ let bgImg;
 let stImg;
 let munchSound;
 let tileOffset = 64;
-
+let upgradeCostMultiplier = 1.15;
 
 let isCursorUpgradeDisplayed = false;
 let isGrandmaUpgradeDisplayed = false;
@@ -30,11 +30,23 @@ let WizardTowerUpgradeCost = 330000000;
 let ShipmentUpgradeCost = 5100000000;
 let AlchemyLabUpgradeCost = 75000000000;
 
+let CursorUpgradeAmount = 0.1;
+let GrandmaUpgradeAmount = 1;
+let FarmUpgradeAmount = 8;
+let MineUpgradeAmount = 47;
+let FactoryUpgradeAmount = 260;
+let BankUpgradeAmount = 1400;
+let TempleUpgradeAmount = 7800;
+let WizardTowerUpgradeAmount = 44000;
+let ShipmentUpgradeAmount = 260000;
+let AlchemyLabUpgradeAmount = 1600000;
+
+
 
 function preload() {
   soundFormats('mp3');
   munchSound = loadSound('clickSound.mp3');
-  
+
   stImg = loadImage('storeTile.png');
   bgImg = loadImage('bgBlue.png');
 }
@@ -46,238 +58,312 @@ function draw() {
 
   displayUpgrades();
 
+// making it so you can hover over items to see what they do
+
+textAlign(RIGHT, CENTER);
+
+  // Original code
+  if (mouseX >= (width - 300) && mouseX <= (width - 300 + stImg.width) && mouseY >= 0 && mouseY <= stImg.height) {
+    text('Adds: ' + CursorUpgradeAmount + ' cookies per second', (width - 310), (height - height) + 32);
+  }
+
+  // Variation 1
+  if (mouseX >= (width - 300) && mouseX <= (width - 300 + stImg.width) && mouseY >= 0 + tileOffset * 1 && mouseY <= stImg.height + tileOffset * 1) {
+    text('Adds: ' + GrandmaUpgradeAmount + ' cookies per second', (width - 310), (height - height) + 32 + tileOffset * 1);
+  }
+
+  // Variation 2
+  if (mouseX >= (width - 300) && mouseX <= (width - 300 + stImg.width) && mouseY >= 0 + tileOffset * 2 && mouseY <= stImg.height + tileOffset * 2) {
+    text('Adds: ' + FarmUpgradeAmount + ' cookies per second', (width - 310), (height - height) + 32 + tileOffset * 2);
+  }
+
+  // Variation 3
+  if (mouseX >= (width - 300) && mouseX <= (width - 300 + stImg.width) && mouseY >= 0 + tileOffset * 3 && mouseY <= stImg.height + tileOffset * 3) {
+    text('Adds: ' + MineUpgradeAmount + ' cookies per second', (width - 310), (height - height) + 32 + tileOffset * 3);
+  }
+
+  // Variation 4
+  if (mouseX >= (width - 300) && mouseX <= (width - 300 + stImg.width) && mouseY >= 0 + tileOffset * 4 && mouseY <= stImg.height + tileOffset * 4) {
+    text('Adds: ' + FactoryUpgradeAmount + ' cookies per second', (width - 310), (height - height) + 32 + tileOffset * 4);
+  }
+
+  // Variation 5
+  if (mouseX >= (width - 300) && mouseX <= (width - 300 + stImg.width) && mouseY >= 0 + tileOffset * 5 && mouseY <= stImg.height + tileOffset * 5) {
+    text('Adds: ' + BankUpgradeAmount + ' cookies per second', (width - 310), (height - height) + 32 + tileOffset * 5);
+  }
+
+  // Variation 6
+  if (mouseX >= (width - 300) && mouseX <= (width - 300 + stImg.width) && mouseY >= 0 + tileOffset * 6 && mouseY <= stImg.height + tileOffset * 6) {
+    text('Adds: ' + TempleUpgradeAmount + ' cookies per second', (width - 310), (height - height) + 32 + tileOffset * 6);
+  }
+
+  if (mouseX >= (width - 300) && mouseX <= (width - 300 + stImg.width) && mouseY >= 0 + tileOffset * 7 && mouseY <= stImg.height + tileOffset * 7) {
+    text('Adds: ' + WizardTowerUpgradeAmount + ' cookies per second', (width - 310), (height - height) + 32 + tileOffset * 7);
+  }
+
+  if (mouseX >= (width - 300) && mouseX <= (width - 300 + stImg.width) && mouseY >= 0 + tileOffset * 8 && mouseY <= stImg.height + tileOffset * 8) {
+    text('Adds: ' + ShipmentUpgradeAmount + ' cookies per second', (width - 310), (height - height) + 32 + tileOffset * 8);
+  }
+
+  if (mouseX >= (width - 300) && mouseX <= (width - 300 + stImg.width) && mouseY >= 0 + tileOffset * 9 && mouseY <= stImg.height + tileOffset * 9) {
+    text('Adds: ' + AlchemyLabUpgradeAmount + ' cookies per second', (width - 310), (height - height) + 32 + tileOffset * 9);
+  }
+
 }
 
 
 
 function displayStats() {
-  textSize(32);
-  textAlign(CENTER, CENTER);
-  text('Cookies: ' + round(cookies, 1) + " 🍪", (width - 300) / 2 , height / 2 - 150);
+  textSize(60);
+  textAlign(LEFT, CENTER);
 
-  textSize(15);
-  textAlign(LEFT, TOP);
-  text("Cookies Per Click: " + round(cookiesPerClick, 1), 5, 5);
+  text('Cookies: 🍪 ' + round(cookies, 1), 5, 30);
 
-  textSize(15);
+  textSize(20);
   textAlign(LEFT, TOP);
-  text("Cookies Per Second: " + round(cookiesPerSecond, 1), 5, 20);
+  text("Cookies Per Second: " + round(cookiesPerSecond, 1), 5, 60);
+  text("Cookies Per Click: " + round(cookiesPerClick, 1), 5, 80);
 }
 
 function displayUpgrades() {
   // Display Grandma upgrade image
-// Original code
-let drawImage = image(stImg, (width-300), (height-height), stImg.width, stImg.height);
+  // Original code
+  let drawImage = image(stImg, (width - 300), (height - height), stImg.width, stImg.height);
 
-// Variation 1
-let drawImage1 = image(stImg, (width-300), (height-height) + tileOffset * 1, stImg.width, stImg.height);
+  // Variation 1
+  let drawImage1 = image(stImg, (width - 300), (height - height) + tileOffset * 1, stImg.width, stImg.height);
 
-// Variation 2
-let drawImage2 = image(stImg, (width-300), (height-height) + tileOffset * 2, stImg.width, stImg.height);
+  // Variation 2
+  let drawImage2 = image(stImg, (width - 300), (height - height) + tileOffset * 2, stImg.width, stImg.height);
 
-// Variation 3
-let drawImage3 = image(stImg, (width-300), (height-height) + tileOffset * 3, stImg.width, stImg.height);
+  // Variation 3
+  let drawImage3 = image(stImg, (width - 300), (height - height) + tileOffset * 3, stImg.width, stImg.height);
 
-// Variation 4
-let drawImage4 = image(stImg, (width-300), (height-height) + tileOffset * 4, stImg.width, stImg.height);
+  // Variation 4
+  let drawImage4 = image(stImg, (width - 300), (height - height) + tileOffset * 4, stImg.width, stImg.height);
 
-// Variation 5
-let drawImage5 = image(stImg, (width-300), (height-height) + tileOffset * 5, stImg.width, stImg.height);
+  // Variation 5
+  let drawImage5 = image(stImg, (width - 300), (height - height) + tileOffset * 5, stImg.width, stImg.height);
 
-// Variation 6
-let drawImage6 = image(stImg, (width-300), (height-height) + tileOffset * 6, stImg.width, stImg.height);
+  // Variation 6
+  let drawImage6 = image(stImg, (width - 300), (height - height) + tileOffset * 6, stImg.width, stImg.height);
 
-// Variation 7
-let drawImage7 = image(stImg, (width-300), (height-height) + tileOffset * 7, stImg.width, stImg.height);
+  // Variation 7
+  let drawImage7 = image(stImg, (width - 300), (height - height) + tileOffset * 7, stImg.width, stImg.height);
 
-// Variation 8
-let drawImage8 = image(stImg, (width-300), (height-height) + tileOffset * 8, stImg.width, stImg.height);
+  // Variation 8
+  let drawImage8 = image(stImg, (width - 300), (height - height) + tileOffset * 8, stImg.width, stImg.height);
 
-// Variation 9
-let drawImage9 = image(stImg, (width-300), (height-height) + tileOffset * 9, stImg.width, stImg.height);
+  // Variation 9
+  let drawImage9 = image(stImg, (width - 300), (height - height) + tileOffset * 9, stImg.width, stImg.height);
 
 
   textSize(26);
   textAlign(LEFT, BOTTOM);
   textFont("Comic Sans MS");
   // Original code
-text('Cursor Upgrade', (width-290), (height-height)+40);
+  text('Cursor Upgrade', (width - 290), (height - height) + 40);
 
-// Variation 1
-text('Grandma Upgrade', (width-290), (height-height)+40 + tileOffset * 1);
+  // Variation 1
+  text('Grandma Upgrade', (width - 290), (height - height) + 40 + tileOffset * 1);
 
-// Variation 2
-text('Farm Upgrade', (width-290), (height-height)+40 + tileOffset * 2);
+  // Variation 2
+  text('Farm Upgrade', (width - 290), (height - height) + 40 + tileOffset * 2);
 
-// Variation 3
-text('Mine Upgrade', (width-290), (height-height)+40 + tileOffset * 3);
+  // Variation 3
+  text('Mine Upgrade', (width - 290), (height - height) + 40 + tileOffset * 3);
 
-// Variation 4
-text('Factory Upgrade', (width-290), (height-height)+40 + tileOffset * 4);
+  // Variation 4
+  text('Factory Upgrade', (width - 290), (height - height) + 40 + tileOffset * 4);
 
-// Variation 5
-text('Bank Upgrade', (width-290), (height-height)+40 + tileOffset * 5);
+  // Variation 5
+  text('Bank Upgrade', (width - 290), (height - height) + 40 + tileOffset * 5);
 
-// Variation 6
-text('Temple Upgrade', (width-290), (height-height)+40 + tileOffset * 6);
+  // Variation 6
+  text('Temple Upgrade', (width - 290), (height - height) + 40 + tileOffset * 6);
 
-// Variation 7
-text('Wizard Tower Upgrade', (width-290), (height-height)+40 + tileOffset * 7);
+  // Variation 7
+  text('Wizard Tower Upgrade', (width - 290), (height - height) + 40 + tileOffset * 7);
 
-// Variation 8
-text('Shipment Upgrade', (width-290), (height-height)+40 + tileOffset * 8);
+  // Variation 8
+  text('Shipment Upgrade', (width - 290), (height - height) + 40 + tileOffset * 8);
 
-// Variation 9
-text('Alchemy Lab Upgrade', (width-290), (height-height)+40 + tileOffset * 9);
+  // Variation 9
+  text('Alchemy Lab Upgrade', (width - 290), (height - height) + 40 + tileOffset * 9);
 
 
-textSize(15);
 
-text('Upgrade Cost: ', (width-290), (height-height)+55);
 
-// Variation 1
-text('Upgrade Cost: ', (width-290), (height-height)+55 + tileOffset * 1);
+  textSize(15);
 
-// Variation 2
-text('Upgrade Cost: ', (width-290), (height-height)+55 + tileOffset * 2);
+  text('Upgrade Cost: ' + round(CursorUpgradeCost), (width - 290), (height - height) + 55);
 
-// Variation 3
-text('Upgrade Cost: ', (width-290), (height-height)+55 + tileOffset * 3);
+  // Variation 1
+  text('Upgrade Cost: ' + round(GrandmaUpgradeCost), (width - 290), (height - height) + 55 + tileOffset * 1);
 
-// Variation 4
-text('Upgrade Cost: ', (width-290), (height-height)+55 + tileOffset * 4);
+  // Variation 2
+  text('Upgrade Cost: ' + round(FarmUpgradeCost), (width - 290), (height - height) + 55 + tileOffset * 2);
 
-// Variation 5
-text('Upgrade Cost: ', (width-290), (height-height)+55 + tileOffset * 5);
+  // Variation 3
+  text('Upgrade Cost: ' + round(MineUpgradeCost), (width - 290), (height - height) + 55 + tileOffset * 3);
 
-// Variation 6
-text('Upgrade Cost: ', (width-290), (height-height)+55 + tileOffset * 6);
+  // Variation 4
+  text('Upgrade Cost: ' + round(FactoryUpgradeCost), (width - 290), (height - height) + 55 + tileOffset * 4);
 
-// Variation 7
-text('Upgrade Cost: ', (width-290), (height-height)+55 + tileOffset * 7);
+  // Variation 5
+  text('Upgrade Cost: ' + round(BankUpgradeCost), (width - 290), (height - height) + 55 + tileOffset * 5);
 
-// Variation 8
-text('Upgrade Cost: ', (width-290), (height-height)+55 + tileOffset * 8);
+  // Variation 6
+  text('Upgrade Cost: ' + round(TempleUpgradeCost), (width - 290), (height - height) + 55 + tileOffset * 6);
 
-// Variation 9
-text('Upgrade Cost: ', (width-290), (height-height)+55 + tileOffset * 9);
-  
+  // Variation 7
+  text('Upgrade Cost: ' + round(WizardTowerUpgradeCost), (width - 290), (height - height) + 55 + tileOffset * 7);
+
+  // Variation 8
+  text('Upgrade Cost: ' + round(ShipmentUpgradeCost), (width - 290), (height - height) + 55 + tileOffset * 8);
+
+  // Variation 9
+  text('Upgrade Cost: ' + round(AlchemyLabUpgradeCost), (width - 290), (height - height) + 55 + tileOffset * 9);
+
 }
 
 function mouseClicked() {
 
-// Original code
-if (mouseX >= (width - 300) && mouseX <= (width - 300 + stImg.width) && mouseY >= 0 && mouseY <= stImg.height) {
-  console.log('Cursor ');
+  // Original code
+  if (mouseX >= (width - 300) && mouseX <= (width - 300 + stImg.width) && mouseY >= 0 && mouseY <= stImg.height) {
+    cursorClicked()
+  }
+
+  // Variation 1
+  if (mouseX >= (width - 300) && mouseX <= (width - 300 + stImg.width) && mouseY >= 0 + tileOffset * 1 && mouseY <= stImg.height + tileOffset * 1) {
+    grandmaClicked()
+  }
+
+  // Variation 2
+  if (mouseX >= (width - 300) && mouseX <= (width - 300 + stImg.width) && mouseY >= 0 + tileOffset * 2 && mouseY <= stImg.height + tileOffset * 2) {
+    farmClicked()
+  }
+
+  // Variation 3
+  if (mouseX >= (width - 300) && mouseX <= (width - 300 + stImg.width) && mouseY >= 0 + tileOffset * 3 && mouseY <= stImg.height + tileOffset * 3) {
+    mineClicked()
+  }
+
+  // Variation 4
+  if (mouseX >= (width - 300) && mouseX <= (width - 300 + stImg.width) && mouseY >= 0 + tileOffset * 4 && mouseY <= stImg.height + tileOffset * 4) {
+    factoryClicked()
+  }
+
+  // Variation 5
+  if (mouseX >= (width - 300) && mouseX <= (width - 300 + stImg.width) && mouseY >= 0 + tileOffset * 5 && mouseY <= stImg.height + tileOffset * 5) {
+    bankClicked()
+  }
+
+  // Variation 6
+  if (mouseX >= (width - 300) && mouseX <= (width - 300 + stImg.width) && mouseY >= 0 + tileOffset * 6 && mouseY <= stImg.height + tileOffset * 6) {
+    templeClicked()
+  }
+
+  if (mouseX >= (width - 300) && mouseX <= (width - 300 + stImg.width) && mouseY >= 0 + tileOffset * 7 && mouseY <= stImg.height + tileOffset * 7) {
+    wizardTowerClicked()
+  }
+
+  if (mouseX >= (width - 300) && mouseX <= (width - 300 + stImg.width) && mouseY >= 0 + tileOffset * 8 && mouseY <= stImg.height + tileOffset * 8) {
+    shipmentClicked()
+  }
+
+  if (mouseX >= (width - 300) && mouseX <= (width - 300 + stImg.width) && mouseY >= 0 + tileOffset * 9 && mouseY <= stImg.height + tileOffset * 9) {
+    alchemyLabClicked()
+  }
 }
-
-// Variation 1
-if (mouseX >= (width - 300) && mouseX <= (width - 300 + stImg.width) && mouseY >= 0 + tileOffset * 1 && mouseY <= stImg.height + tileOffset * 1) {
-  console.log('Granma');
-}
-
-// Variation 2
-if (mouseX >= (width - 300) && mouseX <= (width - 300 + stImg.width) && mouseY >= 0 + tileOffset * 2 && mouseY <= stImg.height + tileOffset * 2) {
-  console.log('Farm');
-}
-
-// Variation 3
-if (mouseX >= (width - 300) && mouseX <= (width - 300 + stImg.width) && mouseY >= 0 + tileOffset * 3 && mouseY <= stImg.height + tileOffset * 3) {
-  console.log('Mine');
-}
-
-// Variation 4
-if (mouseX >= (width - 300) && mouseX <= (width - 300 + stImg.width) && mouseY >= 0 + tileOffset * 4 && mouseY <= stImg.height + tileOffset * 4) {
-  console.log('Factory');
-}
-
-// Variation 5
-if (mouseX >= (width - 300) && mouseX <= (width - 300 + stImg.width) && mouseY >= 0 + tileOffset * 5 && mouseY <= stImg.height + tileOffset * 5) {
-  console.log('Bank');
-}
-
-// Variation 6
-if (mouseX >= (width - 300) && mouseX <= (width - 300 + stImg.width) && mouseY >= 0 + tileOffset * 6 && mouseY <= stImg.height + tileOffset * 6) {
-  console.log('Temple');
-}
-
-if (mouseX >= (width - 300) && mouseX <= (width - 300 + stImg.width) && mouseY >= 0 + tileOffset * 7 && mouseY <= stImg.height + tileOffset * 7) {
-  console.log('Wizard');
-}
-
-if (mouseX >= (width - 300) && mouseX <= (width - 300 + stImg.width) && mouseY >= 0 + tileOffset * 8 && mouseY <= stImg.height + tileOffset * 8) {
-  console.log('Shipment');
-}
-
-if (mouseX >= (width - 300) && mouseX <= (width - 300 + stImg.width) && mouseY >= 0 + tileOffset * 9 && mouseY <= stImg.height + tileOffset * 9) {
-  console.log('Alchemy');
-}
-
-  
-
-
-}
-
-function cursorUpgrade() {
-  cursorButton = createImg('storeTile.png', '');
-  cursorButton.position((width-300), (height-height));
-  cursorButton.mousePressed(cursorClicked);  
-}
-
-
-
 
 function cursorClicked() {
-  cookiesPerSecond += 0.1;
-  cookies
+  if (cookies >= CursorUpgradeCost) {
+    cookies -= CursorUpgradeCost;
+    cookiesPerSecond += CursorUpgradeAmount;
+    CursorUpgradeCost *= upgradeCostMultiplier;
+  }
 }
 
 function grandmaClicked() {
-
+  if (cookies >= GrandmaUpgradeCost) {
+    cookies -= GrandmaUpgradeCost;
+    cookiesPerSecond += GrandmaUpgradeAmount;
+    GrandmaUpgradeCost *= upgradeCostMultiplier;
+  }
 }
 
 function farmClicked() {
-  // Code for farm upgrade clicked
+  if (cookies >= FarmUpgradeCost) {
+    cookies -= FarmUpgradeCost;
+    cookiesPerSecond += FarmUpgradeAmount;
+    FarmUpgradeCost *= upgradeCostMultiplier;
+  }
 }
 
 function mineClicked() {
-  // Code for mine upgrade clicked
+  if (cookies >= MineUpgradeCost) {
+    cookies -= MineUpgradeCost;
+    cookiesPerSecond += MineUpgradeAmount;
+    MineUpgradeCost *= upgradeCostMultiplier;
+  }
 }
 
 function factoryClicked() {
-  // Code for factory upgrade clicked
+  if (cookies >= FactoryUpgradeCost) {
+    cookies -= FactoryUpgradeCost;
+    cookiesPerSecond += FactoryUpgradeAmount;
+    FactoryUpgradeCost *= upgradeCostMultiplier;
+  }
 }
 
 function bankClicked() {
-  // Code for bank upgrade clicked
+  if (cookies >= BankUpgradeCost) {
+    cookies -= BankUpgradeCost;
+    cookiesPerSecond += BankUpgradeAmount;
+    BankUpgradeCost *= upgradeCostMultiplier;
+  }
 }
 
 function templeClicked() {
-  // Code for temple upgrade clicked
+  if (cookies >= TempleUpgradeCost) {
+    cookies -= TempleUpgradeCost;
+    cookiesPerSecond += TempleUpgradeAmount;
+    TempleUpgradeCost *= upgradeCostMultiplier;
+  }
 }
 
 function wizardTowerClicked() {
-  // Code for wizard tower upgrade clicked
+  if (cookies >= WizardTowerUpgradeCost) {
+    cookies -= WizardTowerUpgradeCost;
+    cookiesPerSecond += WizardTowerUpgradeAmount;
+    WizardTowerUpgradeCost *= upgradeCostMultiplier;
+  }
 }
 
 function shipmentClicked() {
-  // Code for shipment upgrade clicked
+  if (cookies >= ShipmentUpgradeCost) {
+    cookies -= ShipmentUpgradeCost;
+    cookiesPerSecond += ShipmentUpgradeAmount;
+    ShipmentUpgradeCost *= upgradeCostMultiplier;
+  }
 }
 
 function alchemyLabClicked() {
-  // Code for alchemy lab upgrade clicked
+  if (cookies >= AlchemyLabUpgradeCost) {
+    cookies -= AlchemyLabUpgradeCost;
+    cookiesPerSecond += AlchemyLabUpgradeAmount;
+    AlchemyLabUpgradeCost *= upgradeCostMultiplier;
+  }
 }
 
+
+
 function autoCookies() {
-  if (frameCount % 60 === 0 && frameCount !== lastSecond) {
-    cookies += cookiesPerSecond;
-    lastSecond = frameCount;
-  }
+  cookies += cookiesPerSecond / 60;
 }
 
 function cookieClicked() {
   cookies += cookiesPerClick;
   munchSound.play()
 }
-
-
