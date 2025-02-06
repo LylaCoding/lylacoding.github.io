da1b7a5a43848c2a872d89a639a4982f3fbb2263
NotLouis05 <90841974+NotLouis05@users.noreply.github.com>
2025-02-06 22:31:25 +0000

    hamborger

--- hamburger.js
+++ hamburger.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
     const hamburger = document.querySelector(".Hamburger");
     const navBoxes = document.querySelector(".NavBoxes");
+    const NavContainer = document.querySelector(".NavContainer");
+    const nav = document.querySelector(".Nav");
 
     hamburger.addEventListener("click", function () {
         navBoxes.classList.toggle("active");
+        nav.classList.toggle("active");
+        hamburger.classList.toggle("active");
     });
 });
